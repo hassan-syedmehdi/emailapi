6949b6d6e98a3ed978008dcad952f44a7ed9e800
Syed Hassan Mehdi Rizvi <syed.r@turvo.com>
2024-07-12 00:42:38 +0530

    Making first commit for web email api using springboot

---
+++ src/main/java/com/email/emailapi/service/EmailService.java
@@ -0,0 +1,11 @@
+package com.email.emailapi.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    
+    public void sendEmail(String subject, String message, String to){
+
+    }
+}
